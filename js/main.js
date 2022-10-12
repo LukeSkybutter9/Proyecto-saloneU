d3dab3a2e9af4de591f436d63c4b220bcdfa26df
LukeSkybutter9 <fali.escalante@gmail.com>
2022-10-12 16:01:18 -0500

    Creando la tabla con Jquery -Header y Footer

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function entrar(){
     var contr = document.getElementById('contraseña').value;
 
     if(user == 'Admin' && contr== '12345'){
-        alert("Bienvenido");
         window.location.href = "index.html";
     }else{
         alert("Ingresa un usuario y contraseña correcto...");
